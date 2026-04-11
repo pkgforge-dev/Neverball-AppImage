994bd900a0f572350169172329ae6de3008b4320
Link4Electronics <link4electronics@gmail.com>
2026-04-11 18:04:51 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,21 +5,19 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/256x256/apps/neverball.png
-export DESKTOP=/usr/share/applications/neverball.desktop
+export ICON=https://raw.githubusercontent.com/Neverball/neverball/refs/heads/master/dist/neverball_256.png
+export DESKTOP=https://raw.githubusercontent.com/Neverball/neverball/refs/heads/master/dist/neverball.desktop.in
 export STARTUPWMCLASS=neverball
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/neverball /usr/bin/neverputt /usr/bin/mapc
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/neverball ./AppDir/bin/neverputt ./AppDir/bin/mapc
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the app normally quits before that time
 # then skip this or check if some flag can be passed that makes it stay open
-#quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
