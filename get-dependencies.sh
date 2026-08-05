daefb776cb1b88fa6f611713dc10c1a87a433510
Link4Electronics <link4electronics@gmail.com>
2026-08-05 05:31:40 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,17 +6,34 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libdecor
+pacman -Syu --noconfirm physfs sdl2_ttf xdg-utils
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
+echo "Building Neverball..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/Neverball/neverball"
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
-    package=neverball-git
+    echo "Making nightly build of Neverball..."
+    echo "---------------------------------------------------------------"
+    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone --depth 1 "$REPO" ./Neverball
 else
-    package=neverball
+    echo "Making stable build of Neverball..."
+    echo "---------------------------------------------------------------"
+    VERSION=1.6.0
+    wget https://neverball.org/neverball-$VERSION.tar.gz
+    mkdir -p ./Neverball && tar -xvf neverball-$VERSION.tar.gz --strip-components=1 -C ./Neverball
 fi
-make-aur-package "$package"
-pacman -Q "$package" | awk '{print $2; exit}' > ~/version
+echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./Neverball
+if [ "${DEVEL_RELEASE-}" != 1 ]; then
+    sed -i 's/^char text_input\[MAXSTR\];/extern char text_input[MAXSTR];/' share/text.h
+    sed -i '/^ifeq ($(ENABLE_FS),stdio)/iENABLE_FS := stdio' Makefile
+fi
+make CPPFLAGS="${CPPFLAGS:-} -DNDEBUG" CFLAGS="${CFLAGS:-} -fcommon" -j$(nproc)
+mv -v neverball neverputt mapc locale data ../AppDir/bin
