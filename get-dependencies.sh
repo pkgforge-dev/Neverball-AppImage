8694319c344bffd8940455eb74a3d90093d3b8f0
Link4Electronics <link4electronics@gmail.com>
2026-08-23 20:24:33 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,13 +7,12 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    libdecor \
     physfs   \
     sdl2_ttf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 # Comment this out if you need an AUR package
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
