fdff4e05521a07df3fb3d2fc8c50d15b94b3244e
Link4Electronics <link4electronics@gmail.com>
2026-08-31 09:34:57 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,7 +7,7 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/256x256/apps/neverball.png
+export ICON=https://raw.githubusercontent.com/Neverball/neverball/refs/heads/master/dist/neverball_256.png
 export DESKTOP=/usr/share/applications/neverball.desktop
 export STARTUPWMCLASS=neverball
 export DEPLOY_OPENGL=1
