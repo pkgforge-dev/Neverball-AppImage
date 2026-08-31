3f5a1faa662d179e914cd2147d7496724a9c49d2
Link4Electronics <link4electronics@gmail.com>
2026-08-31 10:21:51 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,15 +15,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#if [ "${DEVEL_RELEASE-}" = 1 ]; then
-#    package=neverball-git
-#else
-#    package=neverball
-#fi
-#PRE_BUILD_CMDS='sed -i "s/libjpeg/&.so/g" ./PKGBUILD' make-aur-package "$package"
-#pacman -Q "$package" | awk '{print $2; exit}' > ~/version
-
 echo "Building Neverball..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/Neverball/neverball"
