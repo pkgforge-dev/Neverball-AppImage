f70fd035ea3b0118a80bccbbf34ad64ddb814fc5
Link4Electronics <link4electronics@gmail.com>
2026-09-12 01:55:44 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,10 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    physfs    \
-    sdl2_ttf  \
-    xdg-utils
+pacman -Syu --noconfirm physfs sdl2_ttf xdg-utils
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
